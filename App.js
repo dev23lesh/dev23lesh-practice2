9ffb2ea84f3de62b6a73757053e700eed42d5da9
devlesh <dev23lesh@gmail.com>
2019-10-16 14:27:47 +0900

    아이콘추가 및 빌드 수정

--- App.js
+++ App.js
@@ -35,6 +35,7 @@ export default class App extends React.Component {
             returnKeyType={"done"}
             autoCorrect={false}
             onSubmitEditing={this._addToDo}
+            underlineColorAndroid={"transparent"}
             />
             <ScrollView contentContainerStyle={styles.toDos}>
               {Object.values(toDos).reverse().map(toDo=>
@@ -63,7 +64,7 @@ export default class App extends React.Component {
       const toDos = await AsyncStorage.getItem("toDos");
       const parsedToDos = JSON.parse(toDos);
       console.log(toDos);
-      this.setState({loadedToDos:true, toDos:parsedToDos});
+      this.setState({loadedToDos:true, toDos:parsedToDos || {} });
     }catch(err){
       console.log(err);
     }
